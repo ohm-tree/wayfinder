3d1749c462a0ac1277cff52fc9b25ee1c028db17
Rowechen Zhong <rowechen@gmail.com>
2024-10-21 14:56:54 -0400

    I'm not sure what im doing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='wayfinder',
     packages=find_packages(
-        include=['wayfinder', 'wayfinder.*']
+        include=['wayfinder*']
     ),
     author='rowechen',
     author_email='rowechen@mit.edu',
